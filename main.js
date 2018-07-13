9ccfad0fc72133f9e13964a9e2923f905075c853
Bno93 <benno.schweikert@googlemail.com>
2018-07-13 10:44:13 +0200

    disable WindowMenu, remove Links to V1 of Crop planner

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   win = new BrowserWindow({ width: 800, heigh: 600, icon:'images/favicon.png'});
-
+  // comment to debug app
+  // win.setMenu(null);
   win.loadFile('index.html');
 }
 
