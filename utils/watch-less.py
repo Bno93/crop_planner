9086190768d68c7fbd3b6683652f615ff160fe7d
Bno93 <benno.schweikert@googlemail.com>
2018-07-14 14:58:58 +0200

    use whitespace instead of tab

--- utils/watch-less.py
+++ utils/watch-less.py
@@ -4,8 +4,8 @@
 # Auto-compiles style.less to style.css
 #
 # Requires lessc and less clean css to be installed:
-#	npm install -g less
-#	npm install -g less-plugin-clean-css
+#  npm install -g less
+#  npm install -g less-plugin-clean-css
 ################################
 
 import os, time
@@ -16,38 +16,38 @@ from _helper import *
 
 # Main application
 class Main:
-	style_less = "style.less"
-	style_css = "style.css"
-	
-	def __init__(self):
-		clear()
-		os.chdir("../")
-		header("Watching style.less for changes\nctrl+c to exit")
-		print()
-		
-		while True:
-			if not os.path.exists(self.style_less):
-				print(self.style_less + " does not exist. Exiting.")
-				return
-				
-			if not os.path.exists(self.style_css):
-				self.compile()
-			elif path.getmtime(self.style_less) > path.getmtime(self.style_css):
-				self.compile()
-				
-			time.sleep(.2)
-				
-	def compile(self):
-		start = time.time()
-		os.system("lessc " + self.style_less + " " + self.style_css + " --clean-css")
-		touch(self.style_css, path.getmtime(self.style_less))
-		print("Recompiled [" + str(floor((time.time() - start) * 100)) + " ms]")
-		print()
-	
-	
+  style_less = "style.less"
+  style_css = "style.css"
+  
+  def __init__(self):
+    clear()
+    os.chdir("../")
+    header("Watching style.less for changes\nctrl+c to exit")
+    print()
+    
+    while True:
+      if not os.path.exists(self.style_less):
+        print(self.style_less + " does not exist. Exiting.")
+        return
+        
+      if not os.path.exists(self.style_css):
+        self.compile()
+      elif path.getmtime(self.style_less) > path.getmtime(self.style_css):
+        self.compile()
+        
+      time.sleep(.2)
+        
+  def compile(self):
+    start = time.time()
+    os.system("lessc " + self.style_less + " " + self.style_css + " --clean-css")
+    touch(self.style_css, path.getmtime(self.style_less))
+    print("Recompiled [" + str(floor((time.time() - start) * 100)) + " ms]")
+    print()
+  
+  
 # Run application
 if __name__ == "__main__":
-	try:
-		app = Main()
-	except KeyboardInterrupt:
-		print("Exiting")
+  try:
+    app = Main()
+  except KeyboardInterrupt:
+    print("Exiting")
