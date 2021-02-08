cd9bfa35b7b0c6ab5bb85b52f8ad01e392ef2e0f
Bno93 <benno.schweikert@googlemail.com>
2021-02-08 13:36:24 +0100

    Add binary creation package to generate binary app.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, heigh: 600, icon:'images/favicon.png'});
+  let win = new BrowserWindow({ width: 800, heigh: 600, icon:'images/favicon.png'});
   // comment to debug app
   // win.setMenu(null);
   win.loadFile('index.html');
